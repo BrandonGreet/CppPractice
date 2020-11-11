ef7885229f2715b5448abbf38824963ccf6e36ef
Brandon Greet <btg5907@rit.edu>
2020-11-11 11:02:30 -0500

    added ReverseString.cpp

---
+++ ReverseString.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void reverse(char *str)
+{
+    char *end = str;
+    char tmp;
+
+    // Get end of string pointer
+    while (*end) // True as long as *end isn't NULL 
+    {
+        end++;
+    }
+    end--; // address just before NULL character
+
+    while (str < end)
+    {
+        tmp = *str;
+        *str++ = *end;
+        *end-- = tmp;
+    }
+}
+
+int main()
+{
+    char str[27] = "abcdefghijklmnopqrstuvwxyz";
+    reverse(str);
+    for (int i = 0; str[i]; i++)
+    {
+        std::cout << str[i] << std::endl;
+    }
+    return 0;
+}
